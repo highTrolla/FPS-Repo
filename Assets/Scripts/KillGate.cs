a8546349fce298e617de91e1535aaf7df5a688a6
highTrolla <nategibbsremenda@gmail.com>
2020-10-20 15:47:47 -0400

    plz work

---
+++ Assets/Scripts/KillGate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillGate : MonoBehaviour
+{
+    public float unlockCount;
+
+    void Update()
+    {
+        if (Enemy.deathCount >= unlockCount)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WorldManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WorldManager : MonoBehaviour
+{
+
+    public enum Scene
+    {
+        Title,
+        MainLevel,
+    }
+   
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+}
